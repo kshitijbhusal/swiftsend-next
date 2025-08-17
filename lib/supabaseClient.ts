1c6ff8f5a76a2d98a034bfd65bbd327f3161fd3e
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-08-17 20:17:05 +0545

    fix seven

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,5 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-// @ts-expect-error: Supabase types mismatch in server-side client
-export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
