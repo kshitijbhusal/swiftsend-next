ef748fa23d7010137347abf64532ae7704dbc7a3
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2026-01-03 21:03:04 +0545

    dep fix 3

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 import { Send, Download, Sun, Moon } from 'lucide-react';
 
 import { SendBox } from '@/components/SendBox';
-import { ReceiveBox } from '@/components/ReceiveBox';
+import { Receive } from '@/components/Receive';
 
 
 export default function Page() {
